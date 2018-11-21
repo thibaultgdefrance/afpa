b03be8f3cde6293baff269214e8f95840bb83e98
Leto120 <echo_1@hotmail.fr>
2018-11-21 14:13:03 +0100

    ajout css fact

--- exemplefacture.php
+++ exemplefacture.php
@@ -8,7 +8,7 @@
     <title>Panier</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css" >
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/fact.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
