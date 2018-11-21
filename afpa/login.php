99511ef503d531d6d4d88c55e9ce9e72667dccdb
Leto120 <echo_1@hotmail.fr>
2018-11-21 10:41:32 +0100

    login.php meme niveau

---
+++ afpa/login.php
@@ -0,0 +1,10 @@
+<?php
+
+    $login = "admin";
+    $psswd = "admin";
+    if(($_POST['username'] == $login) && ($_POST['psswd'] == $psswd){
+        header('Location: admin.html');
+        exit(); 
+    }else{
+        echo "vous n'avez pas accés à cette page";
+    }
